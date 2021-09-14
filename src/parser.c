1e3108304c6c30ebe317b4bbdeff92df9f3c6bef
Yuval <yuval.tasher@gmail.com>
2021-09-14 16:29:06 +0300

    Verify you can parse_expr in paren parsing

--- src/parser.c
+++ src/parser.c
@@ -278,11 +278,11 @@ static struct Expr *parser_parse_expr_single(struct Parser* const parser) {
         expr->type = ExprTypeSizeof;
         expr->as_single = sizeof_value;
     } else if (parser->lexer.token.name == TokenNameLeftParen) {
-        // FIXME: verify that there is actually an expression
-        expr = parser_parse_expr(parser);
+        if (!(expr = parser_parse_expr(parser))) {
+            parser_error(parser, "Expected an expression after left paren");
+        }
         if (!lexer_tokenize(&parser->lexer) || parser->lexer.token.name != TokenNameRightParen) {
-            lexer_load_state(&parser->lexer, backtrack);
-            return NULL;
+            parser_error(parser, "Unmatched '('");
         }
     } else {
         lexer_load_state(&parser->lexer, backtrack);
