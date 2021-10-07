7e424ab297bc97119c17395c771f1d1ce89d623c
Yuval <yuval.tasher@gmail.com>
2021-10-07 20:08:47 +0300

    Refactor number.c

--- src/number.c
+++ src/number.c
@@ -6,17 +6,15 @@
 
 void interpreter_error(struct Interpreter* const interpreter, struct State state, const char* const error_message);
 
+static inline double number_as_float(struct NumberExpr n) {
+    return n.is_float ? n.as_float : (double)n.as_int;
+}
+
 struct NumberExpr number_add(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
-    if (a.is_float && b.is_float) {
+    if (a.is_float || b.is_float) {
         res.is_float = true;
-        res.as_float = a.as_float + b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.is_float = true;
-        res.as_float = a.as_float + (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.is_float = true;
-        res.as_float = (double)a.as_int + b.as_float;
+        res.as_float = number_as_float(a) + number_as_float(b);
     } else {
         res.is_float = false;
         res.as_int = a.as_int + b.as_int;
@@ -26,15 +24,9 @@ struct NumberExpr number_add(struct NumberExpr a, struct NumberExpr b) {
 
 struct NumberExpr number_sub(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
-    if (a.is_float && b.is_float) {
+    if (a.is_float || b.is_float) {
         res.is_float = true;
-        res.as_float = a.as_float - b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.is_float = true;
-        res.as_float = a.as_float - (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.is_float = true;
-        res.as_float = (double)a.as_int - b.as_float;
+        res.as_float = number_as_float(a) - number_as_float(b);
     } else {
         res.is_float = false;
         res.as_int = a.as_int - b.as_int;
@@ -44,15 +36,9 @@ struct NumberExpr number_sub(struct NumberExpr a, struct NumberExpr b) {
 
 struct NumberExpr number_mul(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
-    if (a.is_float && b.is_float) {
+    if (a.is_float || b.is_float) {
         res.is_float = true;
-        res.as_float = a.as_float * b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.is_float = true;
-        res.as_float = a.as_float * (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.is_float = true;
-        res.as_float = (double)a.as_int * b.as_float;
+        res.as_float = number_as_float(a) * number_as_float(b);
     } else {
         res.is_float = false;
         res.as_int = a.as_int * b.as_int;
@@ -63,21 +49,11 @@ struct NumberExpr number_mul(struct NumberExpr a, struct NumberExpr b) {
 struct NumberExpr number_div(struct Interpreter* const interpreter,
                              struct State state, struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
-    if (a.is_float && b.is_float) {
+    if (a.is_float || b.is_float) {
         res.is_float = true;
-        if (b.as_float == 0.f)
+        if (number_as_float(b) == 0.f)
             interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = a.as_float / b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.is_float = true;
-        if (b.as_int == 0)
-            interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = a.as_float / (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.is_float = true;
-        if (b.as_float == 0.f)
-            interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = (double)a.as_int / b.as_float;
+        res.as_float = number_as_float(a) / number_as_float(b);
     } else {
         div_t division;
         if (b.as_int == 0)
@@ -96,21 +72,11 @@ struct NumberExpr number_div(struct Interpreter* const interpreter,
 
 struct NumberExpr number_mod(struct Interpreter* const interpreter, struct State state, struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
-    if (a.is_float && b.is_float) {
+    if (a.is_float || b.is_float) {
         res.is_float = true;
-        if (b.as_float == 0.f)
+        if (number_as_float(b) == 0.f)
             interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = fmod(a.as_float, b.as_float);
-    } else if (a.is_float && !b.is_float) {
-        res.is_float = true;
-        if (b.as_int == 0)
-            interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = fmod(a.as_float, (double)b.as_int);
-    } else if (!a.is_float && b.is_float) {
-        res.is_float = true;
-        if (b.as_float == 0.f)
-            interpreter_error(interpreter, state, "Division by zero");
-        res.as_float = fmod((double)a.as_int, b.as_float);
+        res.as_float = fmod(number_as_float(a), number_as_float(b));
     } else {
         res.is_float = false;
         if (b.as_int == 0)
@@ -121,26 +87,19 @@ struct NumberExpr number_mod(struct Interpreter* const interpreter, struct State
 }
 
 struct NumberExpr number_neg(struct NumberExpr n) {
-    struct NumberExpr res;
     if (n.is_float) {
-        res.is_float = true;
-        res.as_float = -n.as_float;
+        n.as_float = -n.as_float;
     } else {
-        res.is_float = false;
-        res.as_int = -n.as_int;
+        n.as_int = -n.as_int;
     }
-    return res;
+    return n;
 }
 
 struct NumberExpr number_eq(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
     res.is_float = false;
-    if (a.is_float && b.is_float) {
-        res.as_int = a.as_float == b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.as_int = a.as_float == (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.as_int = (double)a.as_int == b.as_float;
+    if (a.is_float || b.is_float) {
+        res.as_int = number_as_float(a) == number_as_float(b);
     } else {
         res.as_int = a.as_int == b.as_int;
     }
@@ -150,12 +109,8 @@ struct NumberExpr number_eq(struct NumberExpr a, struct NumberExpr b) {
 struct NumberExpr number_smaller(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
     res.is_float = false;
-    if (a.is_float && b.is_float) {
-        res.as_int = a.as_float < b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.as_int = a.as_float < (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.as_int = (double)a.as_int < b.as_float;
+    if (a.is_float || b.is_float) {
+        res.as_int = number_as_float(a) < number_as_float(b);
     } else {
         res.as_int = a.as_int < b.as_int;
     }
@@ -165,12 +120,8 @@ struct NumberExpr number_smaller(struct NumberExpr a, struct NumberExpr b) {
 struct NumberExpr number_bigger(struct NumberExpr a, struct NumberExpr b) {
     struct NumberExpr res;
     res.is_float = false;
-    if (a.is_float && b.is_float) {
-        res.as_int = a.as_float > b.as_float;
-    } else if (a.is_float && !b.is_float) {
-        res.as_int = a.as_float > (double)b.as_int;
-    } else if (!a.is_float && b.is_float) {
-        res.as_int = (double)a.as_int > b.as_float;
+    if (a.is_float || b.is_float) {
+        res.as_int = number_as_float(a) > number_as_float(b);
     } else {
         res.as_int = a.as_int > b.as_int;
     }
