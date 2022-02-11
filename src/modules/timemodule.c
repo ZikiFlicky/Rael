95b581df962b4ea88ff57dbc0e604792a6d8bedc
Yuval <yuval.tasher@gmail.com>
2022-02-11 21:18:52 +0200

    Add :Time:Since function to :Time module
    This function returns the elapsed time since the time given.
    The function basically returns :Time:GetEpoch() - :startTime.
    For example:
    load :Time
    
    :startTime ?= :Time:GetEpoch()
    :DoSomethingPrettyHeavy()
    :timeCommandTook ?= :Time:Since(:startTime)

--- src/modules/timemodule.c
+++ src/modules/timemodule.c
@@ -1,23 +1,44 @@
 #include "rael.h"
 
-RaelValue *module_time_GetEpoch(RaelArgumentList *args, RaelInterpreter *interpreter) {
-    RaelFloat output;
-    RaelFloat millis;
+RaelFloat get_epoch(void) {
     struct timespec tm;
+    RaelFloat time, millis;
 
-    (void)interpreter;
-    assert(arguments_amount(args) == 0);
-
-    // get time into tm
     clock_gettime(CLOCK_REALTIME, &tm);
     // get seconds
-    output = (RaelFloat)tm.tv_sec;
+    time = (RaelFloat)tm.tv_sec;
 
     // get millis and calculate the part after the dot
     millis = (RaelFloat)tm.tv_nsec / 1000000000;
-    output += millis;
+    time += millis;
 
-    return number_newf(output);
+    return time;
+}
+
+RaelValue *module_time_GetEpoch(RaelArgumentList *args, RaelInterpreter *interpreter) {
+    (void)interpreter;
+    assert(arguments_amount(args) == 0);
+    return number_newf(get_epoch());
+}
+
+RaelValue *module_time_Since(RaelArgumentList *args, RaelInterpreter *interpreter) {
+    RaelValue *arg1;
+    RaelFloat old_time, new_time = get_epoch();
+
+    (void)interpreter;
+    assert(arguments_amount(args) == 1);
+    arg1 = arguments_get(args, 0);
+
+    if (arg1->type != &RaelNumberType) {
+        return BLAME_NEW_CSTR_ST("Expected a number", *arguments_state(args, 0));
+    }
+
+    old_time = number_to_float((RaelNumberValue*)arg1);
+    if (old_time < 0 || old_time > new_time) {
+        return BLAME_NEW_CSTR_ST("Invalid time", *arguments_state(args, 0));
+    }
+
+    return number_newf(new_time - old_time);
 }
 
 RaelValue *module_time_Sleep(RaelArgumentList *args, RaelInterpreter *interpreter) {
@@ -58,6 +79,7 @@ RaelValue *module_time_new(RaelInterpreter *interpreter) {
     m = (RaelModuleValue*)module_new(RAEL_HEAPSTR("Time"));
     module_set_key(m, RAEL_HEAPSTR("GetEpoch"), cfunc_new(RAEL_HEAPSTR("GetEpoch"), module_time_GetEpoch, 0));
     module_set_key(m, RAEL_HEAPSTR("Sleep"), cfunc_new(RAEL_HEAPSTR("Sleep"), module_time_Sleep, 1));
+    module_set_key(m, RAEL_HEAPSTR("Since"), cfunc_new(RAEL_HEAPSTR("TimeSince"), module_time_Since, 1));
 
     return (RaelValue*)m;
 }
