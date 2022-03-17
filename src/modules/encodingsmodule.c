1b2a3a4bebc5f1d7a4c8c1b90df7472ad9dce655
Yuval <yuval.tasher@gmail.com>
2022-03-17 22:55:08 +0200

    Add HexEncode and HexDecode functions to :Encodings module

--- src/modules/encodingsmodule.c
+++ src/modules/encodingsmodule.c
@@ -101,6 +101,98 @@ RaelValue *module_encodings_Base64Decode(RaelArgumentList *args, RaelInterpreter
     return string_new_pure(decoded, decoded_length, true);
 }
 
+RaelValue *module_encodings_HexEncode(RaelArgumentList *args, RaelInterpreter *interpreter) {
+    RaelValue *arg;
+    RaelStringValue *string;
+    char *source, *encoded;
+    size_t source_length, encoded_length;
+    bool use_uppercase;
+
+    (void)interpreter;
+    assert(arguments_amount(args) >= 1 && arguments_amount(args) <= 2);
+
+    arg = arguments_get(args, 0);
+    if (arg->type != &RaelStringType)
+        return BLAME_NEW_CSTR_ST("Expected string", *arguments_state(args, 0));
+    string = (RaelStringValue*)arg;
+
+    if (arguments_amount(args) == 2) {
+        arg = arguments_get(args, 1);
+        use_uppercase = value_truthy(arg);
+    } else {
+        use_uppercase = false;
+    }
+
+    source = string->source;
+    source_length = string_length(string);
+
+    encoded_length = source_length * 2;
+    encoded = malloc(encoded_length * sizeof(char));
+
+    for (size_t i = 0; i < source_length; ++i) {
+        char c = source[i];
+        for (size_t j = 0; j < 2; ++j) {
+            char computed = (c & (0xf << ((1 - j) * 4))) >> ((1 - j) * 4);
+            char repr;
+
+            if (computed < 10)
+                repr = '0' + computed;
+            else
+                repr = (use_uppercase ? 'A' : 'a') + (computed - 10);
+            encoded[i * 2 + j] = repr;
+        }
+    }
+
+    return string_new_pure(encoded, encoded_length, true);
+}
+
+RaelValue *module_encodings_HexDecode(RaelArgumentList *args, RaelInterpreter *interpreter) {
+    RaelValue *arg1;
+    RaelStringValue *string;
+    char *source, *decoded;
+    size_t source_length, decoded_length;
+
+    (void)interpreter;
+    assert(arguments_amount(args) == 1);
+
+    arg1 = arguments_get(args, 0);
+    if (arg1->type != &RaelStringType)
+        return BLAME_NEW_CSTR_ST("Expected a string", *arguments_state(args, 0));
+
+    string = (RaelStringValue*)arg1;
+    source = string->source;
+    source_length = string_length(string);
+    if (source_length % 2 != 0)
+        return BLAME_NEW_CSTR_ST("Invalid hex string",
+                                *arguments_state(args, 0));
+
+    decoded_length = source_length / 2;
+    decoded = malloc(decoded_length * sizeof(char));
+
+    for (size_t i = 0; i < decoded_length; ++i) {
+        char c1 = tolower(source[i * 2]), c2 = tolower(source[i * 2 + 1]);
+        char left, right;
+        if (!isxdigit(c1) || !isxdigit(c2)) {
+            free(decoded);
+            return BLAME_NEW_CSTR_ST("Invalid hex string",
+                                    *arguments_state(args, 0));
+        }
+        if (c1 >= 'a')
+            left = c1 - 'a' + 10;
+        else
+            left = c1 - '0';
+
+        if (c2 >= 'a')
+            right = c2 - 'a' + 10;
+        else
+            right = c2 - '0';
+
+        decoded[i] = (left << 4) + right;
+    }
+
+    return string_new_pure(decoded, decoded_length, true);
+}
+
 RaelValue *module_encodings_new(RaelInterpreter *interpreter) {
     RaelModuleValue *m;
 
@@ -109,6 +201,8 @@ RaelValue *module_encodings_new(RaelInterpreter *interpreter) {
 
     module_set_key(m, RAEL_HEAPSTR("Base64Encode"), cfunc_new(RAEL_HEAPSTR("Base64Encode"), module_encodings_Base64Encode, 1));
     module_set_key(m, RAEL_HEAPSTR("Base64Decode"), cfunc_new(RAEL_HEAPSTR("Base64Decode"), module_encodings_Base64Decode, 1));
+    module_set_key(m, RAEL_HEAPSTR("HexEncode"), cfunc_new(RAEL_HEAPSTR("Base64Encode"), module_encodings_HexEncode, 1));
+    module_set_key(m, RAEL_HEAPSTR("HexDecode"), cfunc_ranged_new(RAEL_HEAPSTR("Base64Decode"), module_encodings_HexDecode, 1, 2));
 
     return (RaelValue*)m;
 }
